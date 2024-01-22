19f83ea95cdf46ccba9a259fefe2d09e856838f1
이승상 <soang2533@gmail.com>
2024-01-22 11:56:06 +0900

    first commit

---
+++ src/main/kotlin/org/example/backoffice/BackofficeApplication.kt
@@ -0,0 +1,11 @@
+package org.example.backoffice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackofficeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackofficeApplication>(*args)
+}
