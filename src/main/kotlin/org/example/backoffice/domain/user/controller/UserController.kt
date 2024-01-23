d39fcb6805d842d4c137ac3ce5662f52d7e45912
rim109 <fla7722@naver.com>
2024-01-23 23:36:46 +0900

    [feat] Valid 어노테이션 추가

--- src/main/kotlin/org/example/backoffice/domain/user/controller/UserController.kt
+++ src/main/kotlin/org/example/backoffice/domain/user/controller/UserController.kt
@@ -1,6 +1,7 @@
 package org.example.backoffice.domain.user.controller
 
 import io.swagger.v3.oas.annotations.Operation
+import jakarta.validation.Valid
 import org.example.backoffice.domain.user.dto.*
 import org.example.backoffice.domain.user.service.UserService
 import org.springframework.http.HttpStatus
@@ -52,7 +53,7 @@ class UserController(
     @Operation(summary = "로그인")
     @PostMapping("/login")
     fun login(
-        @RequestBody loginRequest: LoginRequest
+        @Valid @RequestBody loginRequest: LoginRequest
     ): ResponseEntity<LoginResponse> {
         return ResponseEntity
             .status(HttpStatus.OK)
@@ -62,7 +63,7 @@ class UserController(
     @Operation(summary = "회원 가입")
     @PostMapping("/signup")
     fun sighUp(
-        @RequestBody signUpRequest: SighUpRequest
+        @Valid @RequestBody signUpRequest: SighUpRequest
     ): ResponseEntity<UserResponse> {
         return ResponseEntity
             .status(HttpStatus.CREATED)
