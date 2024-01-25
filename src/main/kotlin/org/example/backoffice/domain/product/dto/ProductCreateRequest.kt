f47cb3eb75297265c533b9cf8b96526de96e8e39
rim109 <fla7722@naver.com>
2024-01-25 20:00:45 +0900

    [feat] ProductCreateRequest 수정

--- src/main/kotlin/org/example/backoffice/domain/product/dto/ProductCreateRequest.kt
+++ src/main/kotlin/org/example/backoffice/domain/product/dto/ProductCreateRequest.kt
@@ -2,7 +2,7 @@ package org.example.backoffice.domain.product.dto
 
 
 class ProductCreateRequest(
-    var userId: Long?,
+//    var userId: Long?,
     var name: String?,
     var price: Long?,
     var title: String?,
