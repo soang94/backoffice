9664a5ef19a932356e4f131c19eff16fe44c99b3
rim109 <fla7722@naver.com>
2024-01-27 02:01:50 +0900

    [feat] admin 로직(조회, 등급변경, 삭제) 추가

--- src/main/kotlin/org/example/backoffice/domain/user/service/UserService.kt
+++ src/main/kotlin/org/example/backoffice/domain/user/service/UserService.kt
@@ -1,5 +1,6 @@
 package org.example.backoffice.domain.user.service
 
+import org.example.backoffice.domain.admin.dto.AdminDTO
 import org.example.backoffice.domain.user.dto.*
 
 interface UserService {
@@ -14,4 +15,10 @@ interface UserService {
     fun signUp(request: SighUpRequest): UserResponse
 
     fun changePassword(userId: Long, request: ChangePasswordRequest)
+
+    fun userChangeAdmin(userId: Long, request: AdminDTO): String
+
+    fun deleteUser(userId: Long): String
+
+    fun deleteMyUser(userId: Long): String
 }
